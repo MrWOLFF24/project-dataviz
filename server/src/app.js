dfaee78cde10018a9c4e4cf432418262eefe7a13
MrWOLFF24 <kakashi24@live.fr>
2018-02-20 23:43:06 +0100

    connect to mongo

--- server/src/app.js
+++ server/src/app.js
@@ -2,13 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// connect to mongodb
+mongoose.connect('mongodb://localhost:27017/dataviz');
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback){
+    console.log("Connection Succeeded");
+});
+
+// une cors for cross origin connection
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
+// send data with express
 app.get('/data', (req, res) => {
     res.send(
         [{
@@ -18,4 +29,5 @@ app.get('/data', (req, res) => {
     )
 });
 
+// listen on port 8081
 app.listen(process.env.PORT || 8081);
