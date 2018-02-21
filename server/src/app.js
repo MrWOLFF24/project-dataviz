ce3ecb1ab1aa3c35db135a16cda95cc9285a10f0
MrWOLFF24 <kakashi24@live.fr>
2018-02-21 17:18:20 +0100

    res mongodb

--- server/src/app.js
+++ server/src/app.js
@@ -1,30 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+//const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 const app = express();
 
 // connect to mongodb
-mongoose.connect('mongodb://localhost:27017/dataviz');
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error"));
-db.once("open", function(callback){
-    console.log("Connection Succeeded");
-});
+mongoose.connect('mongodb://localhost:27017', (err, db) => {
+    if (err) throw err;
+    console.log(db);
+}).then(()=> {console.log('connected');})
+  .catch(()=> { console.log(`Error Connecting to the Mongodb Database at URL : mongodb://127.0.0.1:27017`)});
 
-// une cors for cross origin connection
+// use body parser
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use(cors());
+
+// une cors for cross origin connection
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 
 // send data with express
 app.get('/data', (req, res) => {
     res.send(
         [{
             title: "Hello World!",
-            description: "Hi there! How are you?"
+            description: "Hi there! How are you?",
+
         }]
     )
 });
